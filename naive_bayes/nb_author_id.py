0376d9adcd2e9279719f9fb786bd2d33b28ef5aa
Dayyan Smith <dayyan.smith@gmail.com>
2016-05-23 13:52:09 +0200

    Timing Your NB Classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -34,7 +34,10 @@ t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
 prediction = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
 
 from sklearn.metrics import accuracy_score
 
